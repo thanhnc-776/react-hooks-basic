75dc2a33a0e606de7b81ff5622bc4cc6bae718ca
nguyenthanh <ncthanhla@gmail.com>
2020-09-21 16:52:37 +0700

    TodoList + TodoForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import ColorBox from "./components/ColorBox";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "JavaScript" },
+    { id: 2, title: "Java" },
+    { id: 3, title: "ReactJS" },
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function hadleTodoFormSubmit(formValues) {
+    console.log("Form submit: ", formValues);
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React Hooks!</h1>
-      <ColorBox />
+      <h1>React hooks - TodoList</h1>
+      <TodoForm onSubmit={hadleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
